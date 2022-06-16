6690a95898e409b9bacce8f2537eaddc32fced0b
joungeun <lje0818ovo@naver.com>
2022-06-16 16:35:21 +0900

    [INIT]

---
+++ src/main/java/com/dsm/spotemo/SpotEmoApplication.java
@@ -0,0 +1,13 @@
+package com.dsm.spotemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotEmoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotEmoApplication.class, args);
+	}
+
+}
