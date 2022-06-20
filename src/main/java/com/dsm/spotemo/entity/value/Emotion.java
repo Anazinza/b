6ca029d3136e360a68d5e6a62c0ef099d6e4ea45
joungeun <lje0818ovo@naver.com>
2022-06-20 22:29:56 +0900

    [ADD] Constructor to Emotion
    #4

--- src/main/java/com/dsm/spotemo/entity/value/Emotion.java
+++ src/main/java/com/dsm/spotemo/entity/value/Emotion.java
@@ -1,8 +1,13 @@
 package com.dsm.spotemo.entity.value;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Embeddable;
 
 @Embeddable
+@AllArgsConstructor
+@NoArgsConstructor
 public class Emotion {
     private String emotion;
 }
