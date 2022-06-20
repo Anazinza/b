f0a1531a42abf76f98c945af4b3592b93ad4dcf2
joungeun <lje0818ovo@naver.com>
2022-06-20 11:09:18 +0900

    [ADD] wrong pw exception message
    #3

--- src/main/java/com/dsm/spotemo/global/exception/ExceptionMessage.java
+++ src/main/java/com/dsm/spotemo/global/exception/ExceptionMessage.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 @Getter
 public enum ExceptionMessage {
     EmailAlreadyExist(400,"email already exist"),
+    PasswordMisMatch(400, "password mismatch"),
     AccountNotFoundException(404, "account not found"),
     PostNotFoundException(404, "post not found");
 
