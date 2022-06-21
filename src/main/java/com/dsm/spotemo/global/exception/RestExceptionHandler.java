5ebc6ff9b4dbe77dd93fd8973eea9107db374365
joungeun <lje0818ovo@naver.com>
2022-06-21 14:05:40 +0900

    [MODIFY] rest exception response more details
    #7

--- src/main/java/com/dsm/spotemo/global/exception/RestExceptionHandler.java
+++ src/main/java/com/dsm/spotemo/global/exception/RestExceptionHandler.java
@@ -5,12 +5,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.Arrays;
+
 @RestControllerAdvice
 public class RestExceptionHandler {
     @ExceptionHandler
     protected ResponseEntity<RestExceptionResponse> basicExceptionHandler(final BasicException e) {
-        final String message = e.getEm().getMessage();
         final int code = e.getEm().getCode();
-        return new ResponseEntity<>(new RestExceptionResponse(code, message), HttpStatus.valueOf(code));
+        return new ResponseEntity<>(
+                RestExceptionResponse.builder()
+                        .status(e.getEm().getCode())
+                        .message(e.getMessage())
+                        .detailsMessage(e.getDetailsMessage())
+                        .stack(Arrays.stream(e.getStackTrace()).findAny().toString()).build(),
+                HttpStatus.valueOf(code));
     }
 }
--- src/main/java/com/dsm/spotemo/global/exception/RestExceptionResponse.java
+++ src/main/java/com/dsm/spotemo/global/exception/RestExceptionResponse.java
@@ -1,11 +1,15 @@
 package com.dsm.spotemo.global.exception;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@Builder
 public class RestExceptionResponse {
     private final int status;
     private final String message;
+    private final String detailsMessage;
+    private final String stack;
 }
