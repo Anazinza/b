adde44bee21f6da381147623249152692bbb32e4
joungeun <lje0818ovo@naver.com>
2022-06-26 22:28:48 +0900

    [MODIFY] WriteDate not entity
    #11

---
+++ src/main/java/com/dsm/spotemo/entity/value/WriteDate.java
@@ -0,0 +1,20 @@
+package com.dsm.spotemo.entity.value;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+
+import java.util.*;
+
+@AllArgsConstructor
+@Builder
+@Getter
+@NoArgsConstructor
+public class WriteDate {
+    @Id
+    private int id;
+
+    private Set<Integer> years = new HashSet<>();
+    private Set<Integer> months = new HashSet<>();
+    private Set<Date> dates = new HashSet<>();
+    private List<DayPostInfo> dayPostInfos = new ArrayList<>();
+}
--- src/main/java/com/dsm/spotemo/repository/WriteDateRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dsm.spotemo.repository;
-
-import com.dsm.spotemo.entity.WriteDate;
-import org.springframework.data.repository.CrudRepository;
-
-public interface WriteDateRepository extends CrudRepository<WriteDate, Integer> {
-}
