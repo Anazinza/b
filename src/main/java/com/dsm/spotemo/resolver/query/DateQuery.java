49dbf7617f82e2e20ff7d158228d0738cf83272d
joungeun <lje0818ovo@naver.com>
2022-06-26 23:21:02 +0900

    [ADD] graphqls post id int -> string
    #10

--- src/main/java/com/dsm/spotemo/resolver/query/DateQuery.java
+++ src/main/java/com/dsm/spotemo/resolver/query/DateQuery.java
@@ -1,16 +1,15 @@
 package com.dsm.spotemo.resolver.query;
 
-import com.dsm.spotemo.entity.WriteDate;
+import com.dsm.spotemo.entity.value.WriteDate;
 import com.dsm.spotemo.global.auth.AuthenticationFacade;
 import graphql.kickstart.tools.GraphQLQueryResolver;
-import graphql.kickstart.tools.GraphQLResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class DayQuery implements GraphQLQueryResolver {
+public class DateQuery implements GraphQLQueryResolver {
     private final AuthenticationFacade authentication;
 
 
