7ce8c4c564beef752b9c8a3e11385aeb8d2547aa
joungeun <lje0818ovo@naver.com>
2022-07-03 20:26:41 +0900

    [ADD] `@EnableGlobalMethodSecurity`
    #16 addFilter 및 `@PreAuthorize` 활성화

--- src/main/java/com/dsm/spotemo/global/config/SecurityConfiguration.java
+++ src/main/java/com/dsm/spotemo/global/config/SecurityConfiguration.java
@@ -1,16 +1,24 @@
 package com.dsm.spotemo.global.config;
 
+import com.dsm.spotemo.global.auth.AuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration {
+
+    private final AuthenticationFilter authenticationFilter;
 
     @Bean
      SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
@@ -20,8 +28,10 @@ public class SecurityConfiguration {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
-                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll();
-
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                .antMatchers("/**").permitAll()
+                .and()
+                .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
